121acb3c5d790f4abe3f9572e0cbcf39fda49d9a
MurTeana <73140368+MurTeana@users.noreply.github.com>
2022-01-29 14:32:36 +0300

    Добавьте файлы проекта.

---
+++ ACS_Trend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ACS_Trend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
