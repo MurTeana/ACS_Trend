c2a093fd4e3ed2f5630a4ba35534ac059e9f6932
MurTeana <73140368+MurTeana@users.noreply.github.com>
2022-02-01 23:39:20 +0300

    add highchart

---
+++ _ACS_Trend/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _ACS_Trend
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ _ACS_Trend/Models/ViewModels/LineChartVM.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _ACS_Trend.Models.ViewModels
+{
+    public class LineChartVM
+    {
+        public LineChartVM()
+        {
+            title = new Title();
+            subtitle = new Subtitle();
+            xAxis = new XAxis();
+            yAxis = new YAxis();
+            plotOptions = new PlotOptions();
+            series = new Series();
+        }
+        public Title title { get; set; }    
+        public Subtitle subtitle { get; set; }    
+        public XAxis xAxis { get; set; }    
+        public YAxis yAxis { get; set; }    
+        public PlotOptions plotOptions { get; set; }    
+        public Series series { get; set; }    
+    }
+
+    // Title
+    public class Title
+    {
+        public Title()
+        {
+        }
+        public string text { get; set; }
+    }
+
+    // Subtitle
+    public class Subtitle
+    {
+        public Subtitle()
+        {
+            text = "";
+        }
+        public string text { get; set; }
+    }
+
+    // XAxis
+    public class XAxis
+    {
+        public XAxis()
+        {
+            title = new Title();
+        }
+        public Title title { get; set; }
+        public List<string> categories { get; set; }
+    }
+
+    // YAxis
+    public class YAxis
+    {
+        public YAxis()
+        {
+            title = new Title();
+        }
+        public Title title { get; set; }
+        public List<string> categories { get; set; }
+    }
+
+    // Series
+    public class Series
+    {
+        public Series()
+        {
+            data = new List<double[]> ();
+        }
+        public string name { get; set; }
+        public List<double[]> data { get; set; }
+    }
+
+    // PlotOptions
+    public class PlotOptions
+    {
+        public PlotOptions()
+        {
+            line = new Line();
+        }
+        public Line line { get; set; }
+    }
+
+    // PlotOptions: Line
+    public class Line
+    {
+        public Line()
+        {
+            dataLabels = new DataLabels();
+        }
+        public DataLabels dataLabels { get; set; }
+        public bool enableMouseTracking { get; set; }
+    }
+
+    // PlotOptions: DataLabels
+    public class DataLabels
+    {
+        public DataLabels()
+        {
+        }
+        public bool enabled { get; set; }
+    }
+
+}
